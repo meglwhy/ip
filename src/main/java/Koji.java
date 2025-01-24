105273c70e07856d13976a727b6f43a839e92ef4
meglwhy <meglwhy@gmail.com>
2025-01-24 10:34:24 +0800

    level 0

---
+++ src/main/java/Koji.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Koji {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Koji");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        sc.close();
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
