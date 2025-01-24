e18d32a0d57520a3c8dfe67d58d25276720e4e1e
meglwhy <meglwhy@gmail.com>
2025-01-24 10:39:57 +0800

    level 1

--- src/main/java/Koji.java
+++ src/main/java/Koji.java
@@ -8,17 +8,28 @@ public class Koji {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Koji");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        Scanner sc = new Scanner(System.in);
-        sc.nextLine();
-        sc.close();
+        while (true) {
+            String input = sc.nextLine();
 
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        sc.close();
     }
 }
