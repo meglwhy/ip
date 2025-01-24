66bdfbb2a27ec2e361275ba5d28956713ab5b7d5
meglwhy <meglwhy@gmail.com>
2025-01-24 10:59:19 +0800

    level 2

--- src/main/java/Koji.java
+++ src/main/java/Koji.java
@@ -9,6 +9,8 @@ public class Koji {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
+        String[] list = new String[100];
+        int count = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Koji");
@@ -16,20 +18,31 @@ public class Koji {
         System.out.println("____________________________________________________________");
 
         while (true) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
 
             if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                if (count == 0) {
+                    System.out.println("");
+                } else {
+                    for (int i = 0; i < count; i++) {
+                        System.out.println((i + 1) + ". " + list[i]);
+                    }
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                list[count] = input;
+                count++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
-
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
-
         sc.close();
     }
 }
